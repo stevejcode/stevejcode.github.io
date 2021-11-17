bb3db484fa50d7b1920db1c8406c0aae5fe37f5d
stevejcode <stevejcode@gmail.com>
2021-11-16 20:58:47 -0800

    added window.open

--- homework/hw6_html/objects2.js
+++ homework/hw6_html/objects2.js
@@ -32,8 +32,14 @@ const search = () => {
             document.getElementById("original").value = userentry;
             document.getElementById("result").innerHTML = `The letter ${searchChar} was found ${countNumber} times`;
 
-        }
+            if ((countNumber++ === 0)) {
+                var popup = window.open("", "MsgWindow", "width=300, height=100");
+                popup.document.write(`Search character ${searchChar} not found in text string!`);
+                //clear result if 0 text string found?
+                document.getElementById("result").innerHTML = "";
 
+            }
+        }
     }
 
 }
